88173b1908dd20128ff4df666959d577bb94f46f
lmuratore <ltm157m@gmail.com>
2022-09-30 11:09:26 -0400

    Deleted useless lines in test

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -6,7 +6,6 @@ public class TestMapMove extends TestCase {
   public void testPacManInvalidMove1() throws FileNotFoundException {
     NoFrame frame = new NoFrame();
     frame.initPlayers();
-    frame.startGame();
     Map myMap = frame.getMap();
     // try to move pacman to wall
     assertFalse(myMap.move("pacman", new Location(0, 0), Map.Type.PACMAN));
@@ -15,7 +14,6 @@ public class TestMapMove extends TestCase {
   public void testPacManValidMove1() throws FileNotFoundException {
     NoFrame frame = new NoFrame();
     frame.initPlayers();
-    frame.startGame();
     Map myMap = frame.getMap();
     // try to move pacman to empty space
     assertTrue(myMap.move("pacman", new Location(5, 5), Map.Type.PACMAN));
