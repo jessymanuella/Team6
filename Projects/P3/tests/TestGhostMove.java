301df62116695fada9e952bf0db11429c10cb314
Lauren Wei <lwei0973@terpmail.umd.edu>
2022-10-02 19:11:06 -0400

    create initial test for Ghost-move

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -1,7 +1,19 @@
 import java.io.*;
 import junit.framework.*;
+import java.util.ArrayList;
 
 public class TestGhostMove extends TestCase {
 
-  public void testGhostMove() throws FileNotFoundException {}
+  public void testGhostMove() throws FileNotFoundException {
+    Mainframe frame = new MainFrame();
+
+    Ghost ghost = frame.addGhost(new Location(9, 11), "testGhost", Color.red);
+    ArrayList<Location> valid_moves = ghost.get_valid_moves();
+    if (valid_moves.size() == 0) {
+      assertFalse(ghost.move());
+    } else {
+      ghost.move();
+      assertTrue(valid_moves.contains(ghost.myLoc));
+    }
+  }
 }
