72908735fb498961fb52791d8c5dc279b7b9ac67
Vivian Chen <vchen118@gmail.com>
2022-10-02 23:53:40 -0400

    map eatCookie test added

--- Projects/P3/tests/TestMapEatCookie.java
+++ Projects/P3/tests/TestMapEatCookie.java
@@ -2,8 +2,18 @@ import java.io.*;
 import junit.framework.*;
 
 public class TestMapEatCookie extends TestCase {
+  protected void setup(){
+    NoFrame frame = new NoFrame();
+  }
 
   public void testMapEatCookie() throws FileNotFoundException {
-    return null;
+    int num_cookies = frame.getMap().getCookies;
+    String cookie = "tok_x" + 1 + "_y" + 1;
+    JComponent eaten = frame.getMap().eatCookie(cookie);
+    assertTrue(frame.getMap().getCookies == num_cookies - 1);
+    assertTrue(eaten != null);
+    eaten = frame.getMap().eatCookie(cookie);
+    assertTrue(eaten == null);
+    assertTrue(frame.getMap().getCookies == num_cookies - 1);
   }
 }
